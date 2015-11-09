6a775cd522cc6a45e877e949288368aa9a6d43d3
Evgeny Kapun <abacabadabacaba@gmail.com>
2015-11-09 16:45:04 +0300

    Escape markup characters properly in XHTML DTD

--- .entity-to-dtd.pl
+++ .entity-to-dtd.pl
@@ -1,13 +1,15 @@
 #!/usr/bin/perl -wT
 use strict;
 
+sub escape($) { ($_[0] eq "26" || $_[0] eq "3C" ? "&#x26;" : "&") . "#x$_[0];" }
+
 # read dtd
 my $dtd = '';
 while (<>) {
     if (m(<tr(?: id="[^"]+")?> <td> <code data-x="">(.+?);</code> </td> <td> U\+0*([0-9A-F]+) </td>)os) {
-        $dtd .= "<!ENTITY $1 \"&#x$2;\">";
+        $dtd .= "<!ENTITY $1 \"".escape($2)."\">";
     } elsif (m(<tr(?: id="[^"]+")?> <td> <code data-x="">(.+?);</code> </td> <td> U\+0*([0-9A-F]+) U\+0*([0-9A-F]+) </td>)os) {
-        $dtd .= "<!ENTITY $1 \"&#x$2;&#x$3;\">";
+        $dtd .= "<!ENTITY $1 \"".escape($2).escape($3)."\">";
     } else {
         die "$0: line doesn't match pattern:\n$_\n";
     }
