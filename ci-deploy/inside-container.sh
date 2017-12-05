93a6c62afdb9c8cfcfbfa0fecc3102f2630b1810
Philip Jägenstedt <philip@foolip.org>
2017-12-05 09:41:52 +0100

    Stop deploying HTML to old server
    Also remove the *.cgi exclusion, the CGI files have been removed:
    https://github.com/whatwg/html/pull/3198

--- ci-deploy/inside-container.sh
+++ ci-deploy/inside-container.sh
@@ -6,14 +6,9 @@ cd "$(dirname "$0")/../.."
 
 PDF_SOURCE_URL="https://html.spec.whatwg.org/"
 WEB_ROOT="html.spec.whatwg.org"
-DEPLOY_USER="annevankesteren"
 
-SERVER="75.119.197.251"
-SERVER_PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDP7zWfhJdjre9BHhfOtN52v6kIaDM/1kEJV4HqinvLP2hzworwNBmTtAlIMS2JJzSiE+9WcvSbSqmw7FKmNVGtvCd/CNJJkdAOEzYFBntYLf4cwNozCRmRI0O0awTaekIm03pzLO+iJm0+xmdCjIJNDW1v8B7SwXR9t4ElYNfhYD4HAT+aP+qs6CquBbOPfVdPgQMar6iDocAOQuBFBaUHJxPGMAG0qkVRJSwS4gi8VIXNbFrLCCXnwDC4REN05J7q7w90/8/Xjt0q+im2sBUxoXcHAl38ZkHeFJry/He2CiCc8YPoOAWmM8Vd0Ukc4SYZ99UfW/bxDroLHobLQ9Eh"
-
-# New server, see https://github.com/whatwg/misc-server/issues/7
-NEW_SERVER="165.227.248.76"
-NEW_SERVER_PUBLIC_KEY="ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDt6Igtp73aTOYXuFb8qLtgs80wWF6cNi3/AItpWAMpX3PymUw7stU7Pi+IoBJz21nfgmxaKp3gfSe2DPNt06l8="
+SERVER="165.227.248.76"
+SERVER_PUBLIC_KEY="ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDt6Igtp73aTOYXuFb8qLtgs80wWF6cNi3/AItpWAMpX3PymUw7stU7Pi+IoBJz21nfgmxaKp3gfSe2DPNt06l8="
 
 HTML_SHA=$(git -C html rev-parse HEAD)
 
@@ -50,8 +45,8 @@ echo "$SERVER $SERVER_PUBLIC_KEY" > known_hosts
 # Sync, including deletes, but ignoring the commit-snapshots directory so we don't delete that.
 echo "Deploying build output..."
 rsync --rsh="ssh -o UserKnownHostsFile=known_hosts" \
-      --archive --compress --delete --verbose --exclude="commit-snapshots" --exclude="*.cgi" \
-      "$HTML_OUTPUT/" "$DEPLOY_USER@$SERVER:$WEB_ROOT"
+      --archive --compress --delete --verbose --exclude="commit-snapshots" \
+      "$HTML_OUTPUT" "deploy@$SERVER:/var/www/$WEB_ROOT"
 
 # Now sync a commit snapshot
 # (See https://github.com/whatwg/html-build/issues/97 potential improvements to commit snapshots.)
@@ -59,22 +54,7 @@ echo ""
 echo "Deploying commit snapshot..."
 rsync --rsh="ssh -o UserKnownHostsFile=known_hosts" \
       --archive --compress --verbose \
-      "$HTML_OUTPUT/index.html" "$DEPLOY_USER@$SERVER:$WEB_ROOT/commit-snapshots/$HTML_SHA"
-
-# Deploy everything to the new server as well.
-echo "$NEW_SERVER $NEW_SERVER_PUBLIC_KEY" >> known_hosts
-
-echo ""
-echo "Deploying build output to new server..."
-rsync --rsh="ssh -o UserKnownHostsFile=known_hosts" \
-      --archive --compress --delete --verbose --exclude="commit-snapshots" --exclude="*.cgi" \
-      "$HTML_OUTPUT" "deploy@$NEW_SERVER:/var/www/$WEB_ROOT"
-
-echo ""
-echo "Deploying commit snapshot to new server..."
-rsync --rsh="ssh -o UserKnownHostsFile=known_hosts" \
-      --archive --compress --verbose \
-      "$HTML_OUTPUT/index.html" "deploy@$NEW_SERVER:/var/www/$WEB_ROOT/commit-snapshots/$HTML_SHA"
+      "$HTML_OUTPUT/index.html" "deploy@$SERVER:/var/www/$WEB_ROOT/commit-snapshots/$HTML_SHA"
 
 echo ""
 echo "Building PDF..."
@@ -89,14 +69,7 @@ echo ""
 echo "Deploying PDF..."
 rsync --rsh="ssh -o UserKnownHostsFile=known_hosts" \
       --archive --compress --verbose \
-      "$HTML_OUTPUT/print.pdf" "$DEPLOY_USER@$SERVER:$WEB_ROOT/print.pdf"
-
-echo ""
-echo "Deploying PDF to new server..."
-rsync --rsh="ssh -o UserKnownHostsFile=known_hosts" \
-      --archive --compress --verbose \
-      "$HTML_OUTPUT/print.pdf" "deploy@$NEW_SERVER:/var/www/$WEB_ROOT/print.pdf"
-# TODO: deploy PDF to the new server as well
+      "$HTML_OUTPUT/print.pdf" "deploy@$SERVER:/var/www/$WEB_ROOT/print.pdf"
 
 echo ""
 echo "All done!"
